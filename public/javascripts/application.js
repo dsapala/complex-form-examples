517ec0903aa9f838522b103a3bb0f095e59ad0fb
Dan Sapala <dsapala@gmail.com>
2011-07-12 00:41:08 -0400

    adding jQuery equivalents

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -11,3 +11,15 @@ function add_fields(link, association, content) {
     before: content.replace(regexp, new_id)
   });
 }
+
+// jQuery equivalents
+// function remove_fields(link) {
+//   $(link).prev("input[type=hidden]").val("1");
+//   $(link).closest(".fields").hide();
+// }
+// 
+// function add_fields(link, association, content) {
+//   var new_id = new Date().getTime();
+//   var regexp = new RegExp("new_" + association, "g");
+//   $(link).parent().before(content.replace(regexp, new_id));
+// }
